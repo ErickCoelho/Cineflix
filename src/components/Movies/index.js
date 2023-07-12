f4821032dbcf0e61f6083138e533984469c20799
Erick Coelho <erickcoelho06@gmail.com>
2023-07-12 11:09:33 -0300

    display movies from API

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,27 +1,35 @@
 import './style.css';
 import cover1 from '../../assets/img/cover1.png';
-//import axios from 'axios';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Movies(){
-    /*const promiseFilms = axios.get('https://mock-api.driven.com.br/api/v4/cineflex/movies');
-    promiseFilms.then(response =>{
-        const filmsArray = response.data.map(
-            (item) => (
-                <MovieCard imgsrc={item.posterURL} />
-            )
-        );
-    });*/
+    const [filmsArray, setFilmsArray] = useState([]);
+
+    useEffect(() => {
+        const promiseFilms = axios.get('https://mock-api.driven.com.br/api/v4/cineflex/movies');
+        promiseFilms.then(response => {
+            const data = response.data;
+            console.log(data);
+    
+            const movies = data.map(item => (
+                    <MovieCard imgsrc={item.posterURL} />
+                )
+            );
+            setFilmsArray(movies);
+        });
+    
+        console.log('Teste1');
+        console.log(filmsArray);
+        console.log('Teste2');
+    }, []);
+
 
     return(
         <div className="container">
             <div className='text displayNone'>Selecione o Filme</div>
             <div className='containerMovieCards'>
-                
-                <MovieCard imgsrc={cover1}/>
-                <MovieCard imgsrc={cover1}/>
-                <MovieCard imgsrc={cover1}/>
-                <MovieCard imgsrc={cover1}/>
-                <MovieCard imgsrc={cover1}/>
+                {filmsArray}
             </div>
         </div>
     );
